992d2f8b56a2a3f8a15bbb5b927b05d0492e6b5d
Hamza-ye <7amza.it@gmail.com>
2020-04-01 22:21:04 +0300

    first commit

---
+++ src/test/java/com/mass3d/cems/CemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mass3d.cems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CemsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
